765937aa5530870d457fde6dd9087f41d2994a94
honghao.li <honghao.li@11eda05a-b276-4d74-a309-1d70aa31e1c1>
2024-11-13 02:04:29 +0000

    model修改
    git-svn-id: svn://ci.ftxgame.local:8199/repos/ArmChairQuatebackDoc/sourcecode/arm_chair_quaterback@645663 11eda05a-b276-4d74-a309-1d70aa31e1c1

---
+++ lib/common/entities/card_pack_info_entity.dart
@@ -0,0 +1,56 @@
+/*
+ * @Description: 
+ * @Author: lihonghao
+ * @Date: 2024-11-12 17:54:17
+ * @LastEditTime: 2024-11-13 10:03:32
+ */
+import 'package:arm_chair_quaterback/generated/json/base/json_field.dart';
+import 'package:arm_chair_quaterback/generated/json/card_pack_info_entity.g.dart';
+import 'dart:convert';
+
+import 'package:get/get.dart';
+export 'package:arm_chair_quaterback/generated/json/card_pack_info_entity.g.dart';
+
+@JsonSerializable()
+class CardPackInfoEntity {
+  late int rankIndex = 0;
+  late int createTime = 0;
+  late int teamId = 0;
+  late int cardPackGrade = 0;
+  late int freeGiftTime = 0;
+  late int updateTime = 0;
+  late List<CardPackInfoCard> card = [];
+
+  CardPackInfoEntity();
+
+  factory CardPackInfoEntity.fromJson(Map<String, dynamic> json) =>
+      $CardPackInfoEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => $CardPackInfoEntityToJson(this);
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
+}
+
+@JsonSerializable()
+class CardPackInfoCard {
+  late int cardId = 0;
+  late int openTime = 0;
+  late int status = 0;
+  @JSONField(deserialize: false)
+  late RxString remainTime = "".obs;
+
+  CardPackInfoCard({this.status = 0});
+
+  factory CardPackInfoCard.fromJson(Map<String, dynamic> json) =>
+      $CardPackInfoCardFromJson(json);
+
+  Map<String, dynamic> toJson() => $CardPackInfoCardToJson(this);
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
+}
