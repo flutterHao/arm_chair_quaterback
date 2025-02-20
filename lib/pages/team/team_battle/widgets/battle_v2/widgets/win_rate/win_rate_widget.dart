5c156018532a95dc5d05d29369787bf0b562c431
jiahui.ge <jiahui.ge@11eda05a-b276-4d74-a309-1d70aa31e1c1>
2025-02-20 12:43:41 +0000

    fix:修复胜率指示点尾部不显示问题
    git-svn-id: svn://ci.ftxgame.local:8199/repos/ArmChairQuatebackDoc/sourcecode/arm_chair_quaterback@647339 11eda05a-b276-4d74-a309-1d70aa31e1c1

--- lib/pages/team/team_battle/widgets/battle_v2/widgets/win_rate/win_rate_widget.dart
+++ lib/pages/team/team_battle/widgets/battle_v2/widgets/win_rate/win_rate_widget.dart
@@ -374,7 +374,7 @@ class WinRateWidget extends StatelessWidget {
                       ),
                       //实时数据指示点
                       SizedBox(
-                        width: controller.getWidth()-55.w,
+                        width: Utils.getMaxWidth(Get.context!)-55.w,
                         height: controller.getHeight(),
                         child: Stack(
                           children: [
