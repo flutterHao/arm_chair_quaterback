864ef8e8a1f7602537c6316a40cbcfe9348ee45c
jiahui.ge <jiahui.ge@11eda05a-b276-4d74-a309-1d70aa31e1c1>
2025-02-24 09:23:19 +0000

    test:删除测试代码
    git-svn-id: svn://ci.ftxgame.local:8199/repos/ArmChairQuatebackDoc/sourcecode/arm_chair_quaterback@647425 11eda05a-b276-4d74-a309-1d70aa31e1c1

--- lib/pages/team/team_battle/view.dart
+++ lib/pages/team/team_battle/view.dart
@@ -21,8 +21,8 @@ class TeamBattlePage extends StatefulWidget {
 class _TeamBattlePageState extends State<TeamBattlePage> {
 
   late TeamBattleController controller;
-  /// todo test code
-  bool loadData = false;
+  // /// todo test code
+  // bool loadData = false;
   @override
   void initState() {
     super.initState();
@@ -51,15 +51,15 @@ class _TeamBattlePageState extends State<TeamBattlePage> {
     return GetBuilder<TeamBattleController>(
       id: "team_battle",
       builder: (_) {
-        if(!loadData){
-          return Center(
-            child: SizedBox(
-              width: 30,
-              height: 30,
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
+        // if(!loadData){
+        //   return Center(
+        //     child: SizedBox(
+        //       width: 30,
+        //       height: 30,
+        //       child: CircularProgressIndicator(),
+        //     ),
+        //   );
+        // }
         return Obx(() {
           return Stack(
             alignment: Alignment.center,
